34a3323851310ebbdac02b63ab1802788bc1b2d2
Arsh Nadeem <mohdarshnadeem@gmail.com>
2024-09-28 20:10:26 +0530

    Update FactoryProvider.java

--- src/main/java/com/arsh/helper/FactoryProvider.java
+++ src/main/java/com/arsh/helper/FactoryProvider.java
@@ -2,10 +2,11 @@ package com.arsh.helper;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
+//this will provide connection to the database
 public class FactoryProvider {
 	public static SessionFactory factory;
-	
+
+//	getting factory
 	public static SessionFactory getFactory() {
 		if(factory == null) {
 			factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
